1b07c430eb536e9d2122569dfad3961b26e16c63
WaylonMao <william.mao.ca@gmail.com>
2023-11-04 16:54:38 -0600

    Added Orders page

---
+++ RazorPizza/Pages/Orders.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Data;
+using RazorPizza.Models;
+
+namespace RazorPizza.Pages
+{
+    public class Orders : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders { get; set; } = new List<PizzaOrder>();
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public Orders(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void OnGet()
+        {
+            PizzaOrders = _dbContext.PizzaOrders.ToList();
+        }
+    }
+}
